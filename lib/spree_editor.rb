df994e5e026fc32bfcb52c382fb008f0bc03cd89
Kavrus <kavrus@gmail.com>
2013-10-24 12:23:07 +0400

    Update spree_editor.rb

--- lib/spree_editor.rb
+++ lib/spree_editor.rb
@@ -7,7 +7,7 @@ module SpreeEditor
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
+        Rails.application.config.cache_classes ? require(c) : load(c)
       end
     end
 
